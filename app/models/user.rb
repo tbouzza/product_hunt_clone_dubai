34b402e2843d20fe2a767fab1fdcb657735d8087
Tarig Bouazzati <tbouzza@gmail.com>
2020-11-19 10:13:27 +0400

    all good

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # def initialize(attr = {})
+  #   @fist_name = fist_name
+  # end
+
+  def full_name
+    return "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
